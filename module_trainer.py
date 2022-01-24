b9e64001c6865aa715bcf71b078a937a6fd60d03
Ub_eyal <wzwzeyal@gmail.com>
2022-01-24 23:01:59 +0200

    add tensor

--- module_trainer.py
+++ module_trainer.py
@@ -4,12 +4,8 @@ import transformers
 import numpy as np
 from transformers import AdamW, get_linear_schedule_with_warmup
 from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
-<<<<<<< HEAD
 from bert_classifier.module import BertClassifierModule
 from torch.utils.tensorboard import SummaryWriter
-=======
-from bert_classifier_repo.module import BertClassifierModule
->>>>>>> 256f3a9c952e58f4cb83119a61fcaaeece5f7eb4
 import time
 
 
@@ -25,23 +21,7 @@ class bert_classifier_trainer():
         self.loss_fn = nn.CrossEntropyLoss()
         self.tokenizer = transformers.BertTokenizer.from_pretrained(bert_model_name)
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu") 
-<<<<<<< HEAD
         self.writer = SummaryWriter(f'.runs/{bert_model_name}')
-=======
-        self.best_model_name = best_model_name
-
-        # Instantiate Bert Classifier
-        self.bert_classifier = BertClassifierModule(self.bert_model_name, freeze_bert=freeze_bert)
-
-        # Tell PyTorch to run the model on GPU
-        self.bert_classifier.to(self.device)
-
-        # Create the optimizer
-        self.optimizer = AdamW(self.bert_classifier.parameters(),
-                        lr=5e-5,    # Default learning rate
-                        eps=1e-8    # Default epsilon value
-                        )
->>>>>>> 256f3a9c952e58f4cb83119a61fcaaeece5f7eb4
         
     # Create a function to tokenize a set of texts
     def preprocessing_for_bert(self, data):
@@ -256,10 +236,5 @@ class bert_classifier_trainer():
                 print("-"*70)
             print("\n")
         
-<<<<<<< HEAD
         self.writer.flush()
         print("Training complete!")
-=======
-        print("Training complete!")
-        return filename
->>>>>>> 256f3a9c952e58f4cb83119a61fcaaeece5f7eb4
